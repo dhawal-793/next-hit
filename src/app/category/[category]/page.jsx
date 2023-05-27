06bd8454b882b7cd3a6c121b8f857d69ab63312e
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-05-27 10:17:20 +0530

    Updated category/[category] route to display dynamic data.

--- src/app/category/[category]/page.jsx
+++ src/app/category/[category]/page.jsx
@@ -1,7 +1,18 @@
+import { notFound } from 'next/navigation.js'
+import CardContainer from '@/src/components/CardContainer.jsx'
+import categories from '@/src/data/constants.js'
+import products from "@/src/DB/product.json"
 
-const CategoryPage = () => {
+const CategoryPage = ({ params: { category } }) => {
+
+    if (!categories.includes(category)) { return notFound() }
+
+    const data = products.filter(
+        (el) => el.category.toLowerCase().includes(category)
+    )
+
     return (
-        <div>Category</div>
+        <CardContainer productsData={data} />
     )
 }
 
