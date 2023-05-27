96fe9122b9d1b864d7d90f62f40202c11bf89b86
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-05-27 10:28:20 +0530

    Added routing functioality switched Link to button with appropriate functions

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,28 @@
-import Link from "next/link"
 import { RiHome4Fill, RiBookmarkFill } from "react-icons/ri"
 import ButtonLinks from "../data/categories"
+import { useRouter } from "next/navigation"
 
+const Sidebar = ({ navOpen = null, setNavOpen = null }) => {
+  const router = useRouter();
 
+  const handleLink = ({ category, href }) => {
+    if (category)
+      router.push(category === "all" ? '/' : `/category/${category}`)
 
-const Sidebar = ({ navOpen }) => {
+    if (href)
+      router.push(href)
+
+    setNavOpen(prev => !prev)
+  }
   return (
     <nav id="sidebar" className={`fixed left-0 top-14 sm:top-[70px] h-screen transition-all duration-300 delay-100 ease-in-out ${!navOpen ? "-translate-x-full" : "translate-x-0"} bg-dark-secondary overflow-auto text-light-primary uppercase z-50`}>
       <div className="w-full bg-dark-primary">
         <ul className="flex justify-between p-2 py-4">
           <li>
-            <Link href="/" className="sidebarLinks"><RiHome4Fill className="text-lg md:text-2xl" />Home</Link>
+            <button onClick={() => handleLink({ href: "/" })} className="sidebarLinks"><RiHome4Fill className="text-lg md:text-2xl" />Home</button>
           </li>
           <li>
-            <Link href="/bookmarks" className="sidebarLinks"><RiBookmarkFill className="text-lg md:text-2xl" />Bookmarks</Link>
+            <button onClick={() => handleLink({ href: "/bookmarks" })} className="sidebarLinks"><RiBookmarkFill className="text-lg md:text-2xl" />Bookmarks</button>
           </li>
         </ul>
       </div>
@@ -21,14 +30,13 @@ const Sidebar = ({ navOpen }) => {
         {ButtonLinks.map((buttonLink) => {
           const Icon = buttonLink.icon[0]
           return (
-            <Link
-              href={`/category/${buttonLink.category}`}
-              className={`pr-3 py-3 pl-5 sm:pl-10 w-full flex items-center gap-4 border-y border-b-[#535353] border-t-[#ffffff1a] ${buttonLink.id !== 1 && "border-t-0"} transition-all duration-300 hover:shadow-[0_0_10px_3px_rgba(34,34,34,1)] hover:border-y-transparent hover:z-10`}
+            <buton onClick={() => handleLink({ category: buttonLink.category })}
+              className={`pr-3 py-3 pl-5 cursor-pointer sm:pl-10 w-full flex items-center gap-4 border-y border-b-[#535353] border-t-[#ffffff1a] ${buttonLink.id !== 1 && "border-t-0"} transition-all duration-300 hover:shadow-[0_0_10px_3px_rgba(34,34,34,1)] hover:border-y-transparent hover:z-10`}
               key={buttonLink.id}
             >
               <Icon className="w-5 h-5" />
               <span className="">{buttonLink.name}</span>
-            </Link>
+            </buton>
           )
         })}
       </ul>
