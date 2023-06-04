e6ebe94fd7bf1537091a91fd5118cfd2fc112ce8
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-04 18:30:54 +0530

    Updated Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,16 @@
+import { RiBookmarkFill } from "react-icons/ri"
 
 
 const Button = ({ handleClick, text }) => {
     return (
-        <button onClick={handleClick} className="capitalize bg-dark-primary text-light-primary py-2 px-6 rounded-lg m-0.5 cursor-pointer hover:scale-105 transition-all">
-            {text}
+        <button onClick={handleClick} className="capitalize bg-dark-primary text-light-primary py-2 px-6 rounded-lg m-0.5 cursor-pointer hover:scale-105 transition-all flex items-center space-x-2">
+            <span>
+                {text}
+            </span>
+            {
+                text === "Remove" &&
+                    <RiBookmarkFill className="w-4 h-4"/>
+            }
         </button>
     )
 }
