82273ffa7d1cf270be08cdb6bfaeed93f53715a7
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-07 08:23:24 +0530

    Updated font sixe for h2

--- src/app/community/page.jsx
+++ src/app/community/page.jsx
@@ -1,3 +1,5 @@
+
+
 import fetchContributers from "@/utils/fetchContributers"
 
 const Community = async () => {
@@ -5,7 +7,7 @@ const Community = async () => {
 
   return (
     <div className="px-2 py-4 bg-wite">
-      <h2 className="font-semibold text-2xl sm:text-3xl md:text-[3rem] text-center my-2 mb-10">Meet Our Contributors</h2>
+      <h2 className="font-semibold text-2xl sm:text-4xl md:text-[3rem] text-center my-2 mb-10">Meet Our Contributors</h2>
 
       <ul className="relative flex flex-wrap justify-center w-full gap-12 py-7">
         {contributors?.map(
@@ -17,7 +19,7 @@ const Community = async () => {
                   className="block w-20 xs:w-[90px] md:w-[100px] h-20 xs:h-[90px] md:h-[100px] rounded-full "
                   href={contributor.html_url}
                   target="_blank"
-                  title={`${contributor.login}`}
+                  title={contributor.login}
                 >
                   <img
                     alt={contributor.login}
