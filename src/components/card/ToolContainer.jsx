9c0ed7796c9d3c62976866917e044c0a03a9c52a
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-09 08:43:48 +0530

    Added FilterButtonGroup and ViewButtonGroup in ToolContainer

--- src/components/card/ToolContainer.jsx
+++ src/components/card/ToolContainer.jsx
@@ -1,10 +1,10 @@
 'use client'
 
 import { useState } from "react"
-import { MdWindow } from "react-icons/md"
-import { TfiMenuAlt } from "react-icons/tfi"
 import Card from "./Card"
 import List from "./List"
+import ViewButtonGroup from "../buttons/ViewButtonGroup"
+import FilterButtonGroup from "../buttons/FilterButtonGroup"
 
 const ToolContainer = ({ productsData }) => {
 
@@ -14,9 +14,10 @@ const ToolContainer = ({ productsData }) => {
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="flex items-center justify-end w-full gap-4 p-5 pt-8">
-        <MdWindow role="button" onClick={() => toggleLayout("card")} className={`w-7 h-7 xs:w-8 xs:h-8 cursor-pointer border-b-4 transition-all duration-200 ${layout === "card" ? "text-dark-primary border-dark-primary" : "text-dark-secondary opacity-70 border-transparent"} hover:text-dark-primary  hover:opacity-100`} />
-        <TfiMenuAlt role="button" onClick={() => toggleLayout("list")} className={`w-7 h-7 xs:w-8 xs:h-8 cursor-pointer border-b-4 transition-all duration-200 ${layout === "list" ? "text-dark-primary border-dark-primary" : "text-dark-secondary opacity-70 border-transparent"} hover:text-dark-primary  hover:opacity-100`} />
+
+      <div className="flex justify-between w-full p-5 pt-8">
+        <FilterButtonGroup />
+        <ViewButtonGroup layout={layout} toggleLayout={toggleLayout} />
       </div>
 
       <div className={`grid items-center justify-center w-full gap-4 p-5  ${layout === "card" && "grid-view"} page-height md:md-page-height`}>
