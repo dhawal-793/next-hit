a3e260feda86dc49f3d7d946b457e9b960e462e2
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-09 08:25:29 +0530

    Added useProductsContext on category page

--- src/app/category/[category]/page.jsx
+++ src/app/category/[category]/page.jsx
@@ -1,16 +1,18 @@
+'use client'
+
 import { notFound } from 'next/navigation.js'
 import ToolContainer from '@/components/card/ToolContainer.jsx'
 import categories from '@/data/constants.js'
-import products from "@/DB/product.json"
+import { useProductsContext } from '@/context/productsContext'
 
 const CategoryPage = ({ params: { category } }) => {
+const {products}=useProductsContext()
 
     if (!categories.includes(category)) { return notFound() }
 
     const data = category === "all" ? products : products.filter(
         (el) => el.category.toLowerCase().includes(category)
     )
-
     return (
         <ToolContainer productsData={data} />
     )
