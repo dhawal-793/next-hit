5b06fc1ac70537b7c28001cbb21b1578b0e03ffd
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-10 14:53:56 +0530

    Added condition to show sad Face when no data.

--- src/components/card/ToolContainer.jsx
+++ src/components/card/ToolContainer.jsx
@@ -15,17 +15,29 @@ const ToolContainer = ({ productsData }) => {
   return (
     <div className="flex flex-col items-center justify-center">
 
-      <div className="flex justify-between w-full p-5 pt-8">
-        <FilterButtonGroup />
-        <ViewButtonGroup layout={layout} toggleLayout={toggleLayout} />
-      </div>
+      {productsData.length > 0 ?
+        <>
 
-      <div className={`grid items-center justify-center w-full gap-4 p-5  ${layout === "card" && "grid-view"} page-height md:md-page-height`}>
-        {productsData.map((productData, index) => {
-          return layout === "card" ? <Card key={index} productData={productData} /> :
-            <List key={index} productData={productData} />
-        })}
-      </div>
+          <div className="flex justify-between w-full p-5 pt-8">
+            <FilterButtonGroup />
+            <ViewButtonGroup layout={layout} toggleLayout={toggleLayout} />
+          </div>
+
+          <div className={`grid items-center justify-center w-full gap-4 p-5  ${layout === "card" && "grid-view"}`}>
+            {productsData.map((productData, index) => {
+              return layout === "card" ? <Card key={index} productData={productData} /> :
+                <List key={index} productData={productData} />
+            })}
+          </div>
+        </>
+        :
+        <div className="flex flex-col items-center p-12 pb-1 text-center gap-y-5">
+          <p className="py-5 text-[2rem] font-bold uppercase ">
+            sorry, out toolbox seems empty for this Search term!
+          </p>
+          <img src="/images/sad-face-2.png" alt="Tools Not Found" className="h-60 w-52"/>
+        </div>
+      }
     </div>
   )
 }
