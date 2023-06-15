370431e641bb0687d0f94651e47e8f78b6a94026
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-15 13:55:09 +0530

    Added condition to hide sort buttons on /bookmarks

--- src/components/card/ToolContainer.jsx
+++ src/components/card/ToolContainer.jsx
@@ -1,29 +1,30 @@
 'use client'
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Card from "./Card"
 import List from "./List"
 import ViewButtonGroup from "../buttons/ViewButtonGroup"
 import FilterButtonGroup from "../buttons/FilterButtonGroup"
 import NoDataFound from "../NoDataFound"
+import { usePathname } from "next/navigation"
 
 const ToolContainer = ({ productsData }) => {
+  const pathName = usePathname()
 
   const [layout, setLayout] = useState("card")
 
   const toggleLayout = (type) => setLayout(type)
 
+  useEffect(() => {}, [pathName])
+
   return (
     <div className="flex flex-col items-center justify-center">
-
       {productsData?.length > 0 ?
         <>
-
           <div className="flex justify-between w-full p-5 pt-8">
-            <FilterButtonGroup />
             <ViewButtonGroup layout={layout} toggleLayout={toggleLayout} />
+            {pathName !== "/bookmarks" && <FilterButtonGroup />}
           </div>
-
           <div className={` items-center justify-center w-full gap-4 p-5  ${layout === "card" ? "grid grid-view" : "flex flex-col "}`}>
             {productsData.map((productData) => {
               return layout === "card" ? <Card key={productData.productName} productData={productData} /> :
