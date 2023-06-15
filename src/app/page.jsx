d34d60fda06270dd1b1066c8e34f9f2b03f360c9
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-15 09:18:08 +0530

    Added logic to filter data based on searchParams

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,16 @@
 'use client'
 import ToolContainer from "@/components/card/ToolContainer";
 import { useProductsContext } from "@/context/productsContext";
+import { searchProducts } from "@/utils/search_sort";
+import { useSearchParams } from "next/navigation";
 
 
 export default function Home() {
     const { products } = useProductsContext()
-
+    const searchParams = useSearchParams()
+    const term = searchParams.get('term') || ""
+    const filteredProducts = searchProducts(products, term)
     return (
-        <ToolContainer productsData={products} />
+        <ToolContainer productsData={filteredProducts} />
     )
 }
