6c7b6ad777993c9c66d9ec2bb4c96c8dc80951f3
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-16 11:01:43 +0530

    Updatde Category page to use filteredProducts.

--- src/app/category/[category]/page.jsx
+++ src/app/category/[category]/page.jsx
@@ -1,26 +1,16 @@
 'use client'
 
-import { notFound, useSearchParams } from 'next/navigation'
+import { notFound } from 'next/navigation'
 import ToolContainer from '@/components/card/ToolContainer.jsx'
 import categories from '@/data/constants.js'
 import { useProductsContext } from '@/context/productsContext'
-import { searchProducts } from '@/utils/search_sort'
+
 
 const CategoryPage = ({ params: { category } }) => {
     if (!categories.includes(category)) { return notFound() }
-
-    const { products } = useProductsContext()
-    const searchParams = useSearchParams()
-    const term = searchParams.get('term') || ""
-
-    const filteredProducts = category === "all" ? products : products.filter(
-        (el) => el.category.toLowerCase() === category
-    )
-
-    const searchedFilteredProducts = searchProducts(filteredProducts, term)
-
+    const { filteredProducts } = useProductsContext()
     return (
-        <ToolContainer productsData={searchedFilteredProducts} />
+        <ToolContainer productsData={filteredProducts} />
     )
 }
 
