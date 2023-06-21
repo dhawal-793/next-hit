33464d9c629b131012020c7966c017134f4ff7a2
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-21 16:30:47 +0530

    Updated loader props

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,9 @@
 import { CgSpinnerTwoAlt } from 'react-icons/cg'
 
-const Loader = ({ width = "w-8", height = "h-8" }) => {
+const Loader = ({ width = "w-8", height = "h-8" ,color="text-dark-primary"}) => {
     return (
-        <div className="grid my-4 place-items-center text-dark-primary ">
-            <CgSpinnerTwoAlt className={` ${width} ${height} animate-spin`} />
+        <div className="grid my-4 place-items-center ">
+            <CgSpinnerTwoAlt className={` ${width} ${height} ${color}  animate-spin`} />
         </div>
     )
 }
