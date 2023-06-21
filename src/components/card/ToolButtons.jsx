a2193ec0a99f0241bd64e431d178ee6fdd6914a0
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-21 16:33:50 +0530

    Bug fixed for onclick event

--- src/components/card/ToolButtons.jsx
+++ src/components/card/ToolButtons.jsx
@@ -6,23 +6,22 @@ import Button from "./Button"
 const ToolButtons = ({ link, productData, productName }) => {
     const { bookmarks, addBookmark, removeBookmark } = useBookmarkContext()
     const path = usePathname()
+
+    const handleClick = (e, type) => {
+        e.stopPropagation()
+        type === "add" ? addBookmark(productData) : removeBookmark(productData)
+    }
     return (
         <div className="flex mx-auto gap-x-2 w-fit">
-            <a href={link} target="_blank" className="capitalize bg-dark-primary text-light-primary py-2 px-6 rounded-lg m-0.5 cursor-pointer hover:scale-105 inline-block transition-all">
+            <a href={link} onClick={(e) => e.stopPropagation()} target="_blank" className="capitalize bg-dark-primary text-light-primary py-2 px-6 rounded-lg m-0.5 cursor-pointer hover:scale-105 inline-block transition-all">
                 Visit
             </a>
 
-            {path === "/bookmarks" ? <Button handleClick={() => removeBookmark(productData)} text="Remove" /> :
+            {path === "/bookmarks" ? <Button handleClick={(e) => handleClick(e, "remove")} text="Remove" /> :
                 bookmarks.some((obj) => obj['productName'] === productName) ?
-                    <Button text="Remove" handleClick={(e) => {
-                        e.stopPropagation()
-                        removeBookmark(productData)
-                    }} />
+                    <Button text="Remove" handleClick={(e) => handleClick(e, "remove")} />
                     :
-                    <Button text="Bookmark" handleClick={(e) => {
-                        e.stopPropagation()
-                        addBookmark(productData)
-                    }} />
+                    <Button text="Bookmark" handleClick={(e) => handleClick(e, "add")} />
             }
         </div>
     )
